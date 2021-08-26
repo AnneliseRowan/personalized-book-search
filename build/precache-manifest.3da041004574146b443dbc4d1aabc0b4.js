25d9e753475b2b4b0c3e6c9a14c4db228f053971
Annélise Rowan <40217057+AnneliseRowan@users.noreply.github.com>
2021-08-26 13:43:26 -0400

    added build folder

---
+++ build/precache-manifest.3da041004574146b443dbc4d1aabc0b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58166b1271068900d3c61085ac6affa6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "276c358f5f205512dab4",
+    "url": "/static/css/2.fa6c921b.chunk.css"
+  },
+  {
+    "revision": "ced51931c19f02d079d7",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "276c358f5f205512dab4",
+    "url": "/static/js/2.1ed98e64.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.1ed98e64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ced51931c19f02d079d7",
+    "url": "/static/js/main.a7f65018.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
